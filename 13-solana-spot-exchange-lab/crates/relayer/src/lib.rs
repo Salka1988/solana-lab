d74d016a257dfbe7ea36dea3f1bababbc853de01
Emir Salkic <emirsalkicart@gmail.com>
2026-08-14 14:56:04 +0200

    add relayer transaction assembly

--- 13-solana-spot-exchange-lab/crates/relayer/src/lib.rs
+++ 13-solana-spot-exchange-lab/crates/relayer/src/lib.rs
@@ -6,11 +6,17 @@ use settlement_client::{
     signed_settlement_flow_transaction_instructions, CancelSignedOrderFlowAccounts,
     ComputeBudgetPreset, SignedSettlementFlowAccounts,
 };
+use solana_hash::Hash;
+use solana_keypair::Keypair;
+use solana_message::{Message, VersionedMessage};
+use solana_signer::Signer;
+use solana_transaction::versioned::VersionedTransaction;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct InstructionPlan {
     pub instructions: Vec<Instruction>,
     pub required_signers: Vec<Pubkey>,
+    pub fee_payer: Pubkey,
 }
 
 pub type TransactionSignature = [u8; 64];
@@ -22,6 +28,9 @@ pub struct SubmittedTransaction {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SubmissionError {
+    BlockhashUnavailable(String),
+    BuildTransaction(String),
+    MissingRequiredSigner(Pubkey),
     Rejected(String),
 }
 
@@ -51,6 +60,101 @@ impl SolanaSubmitter for RecordingSubmitter {
             signature: self.signature,
         })
     }
+}
+
+pub trait BlockhashProvider {
+    fn latest_blockhash(&mut self) -> Result<Hash, SubmissionError>;
+}
+
+pub trait TransactionSigner {
+    fn sign(
+        &self,
+        plan: &InstructionPlan,
+        recent_blockhash: Hash,
+    ) -> Result<VersionedTransaction, SubmissionError>;
+}
+
+pub trait TransactionSender {
+    fn send(
+        &mut self,
+        transaction: VersionedTransaction,
+    ) -> Result<SubmittedTransaction, SubmissionError>;
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RpcSubmitter<B, S, T> {
+    blockhash_provider: B,
+    transaction_signer: S,
+    transaction_sender: T,
+}
+
+impl<B, S, T> RpcSubmitter<B, S, T> {
+    pub const fn new(blockhash_provider: B, transaction_signer: S, transaction_sender: T) -> Self {
+        Self {
+            blockhash_provider,
+            transaction_signer,
+            transaction_sender,
+        }
+    }
+}
+
+impl<B, S, T> SolanaSubmitter for RpcSubmitter<B, S, T>
+where
+    B: BlockhashProvider,
+    S: TransactionSigner,
+    T: TransactionSender,
+{
+    fn submit(&mut self, plan: InstructionPlan) -> Result<SubmittedTransaction, SubmissionError> {
+        let blockhash = self.blockhash_provider.latest_blockhash()?;
+        let transaction = self.transaction_signer.sign(&plan, blockhash)?;
+        self.transaction_sender.send(transaction)
+    }
+}
+
+#[derive(Debug)]
+pub struct InMemoryTransactionSigner {
+    signers: Vec<Keypair>,
+}
+
+impl InMemoryTransactionSigner {
+    pub const fn new(signers: Vec<Keypair>) -> Self {
+        Self { signers }
+    }
+}
+
+impl TransactionSigner for InMemoryTransactionSigner {
+    fn sign(
+        &self,
+        plan: &InstructionPlan,
+        recent_blockhash: Hash,
+    ) -> Result<VersionedTransaction, SubmissionError> {
+        let message = Message::new_with_blockhash(
+            &plan.instructions,
+            Some(&plan.fee_payer),
+            &recent_blockhash,
+        );
+        let signers = required_keypairs(&self.signers, &plan.required_signers)?;
+
+        VersionedTransaction::try_new(VersionedMessage::Legacy(message), &signers)
+            .map_err(|error| SubmissionError::BuildTransaction(error.to_string()))
+    }
+}
+
+fn required_keypairs<'a>(
+    available: &'a [Keypair],
+    required: &[Pubkey],
+) -> Result<Vec<&'a Keypair>, SubmissionError> {
+    let mut keypairs = Vec::with_capacity(required.len());
+
+    for required_signer in required {
+        let keypair = available
+            .iter()
+            .find(|keypair| keypair.pubkey() == *required_signer)
+            .ok_or(SubmissionError::MissingRequiredSigner(*required_signer))?;
+        keypairs.push(keypair);
+    }
+
+    Ok(keypairs)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -96,6 +200,7 @@ impl SettlementInstructionPlanner {
         InstructionPlan {
             instructions,
             required_signers: unique_signers([request.settlement_authority, request.payer]),
+            fee_payer: request.payer,
         }
     }
 
@@ -115,6 +220,7 @@ impl SettlementInstructionPlanner {
         InstructionPlan {
             instructions,
             required_signers: unique_signers([request.trader, request.payer]),
+            fee_payer: request.payer,
         }
     }
 }
@@ -156,8 +262,51 @@ fn unique_signers<const N: usize>(signers: [Pubkey; N]) -> Vec<Pubkey> {
 mod tests {
     use super::*;
 
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    struct FixedBlockhashProvider(Result<Hash, SubmissionError>);
+
+    impl BlockhashProvider for FixedBlockhashProvider {
+        fn latest_blockhash(&mut self) -> Result<Hash, SubmissionError> {
+            self.0.clone()
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    struct RecordingTransactionSender {
+        result: Result<SubmittedTransaction, SubmissionError>,
+        sent_count: usize,
+    }
+
+    impl RecordingTransactionSender {
+        const fn new(result: Result<SubmittedTransaction, SubmissionError>) -> Self {
+            Self {
+                result,
+                sent_count: 0,
+            }
+        }
+    }
+
+    impl TransactionSender for RecordingTransactionSender {
+        fn send(
+            &mut self,
+            transaction: VersionedTransaction,
+        ) -> Result<SubmittedTransaction, SubmissionError> {
+            assert!(!transaction.signatures.is_empty());
+            self.sent_count += 1;
+            self.result.clone()
+        }
+    }
+
     fn pubkey(byte: u8) -> Pubkey {
         Pubkey::new_from_array([byte; 32])
+    }
+
+    fn keypair(byte: u8) -> Keypair {
+        Keypair::new_from_array([byte; 32])
+    }
+
+    fn blockhash(byte: u8) -> Hash {
+        Hash::new_from_array([byte; 32])
     }
 
     fn signed_order(
@@ -325,5 +474,182 @@ mod tests {
 
         assert_eq!(submitted.signature, [43; 64]);
         assert_eq!(submitter.submitted_plans, vec![plan]);
+    }
+
+    #[test]
+    fn rpc_submitter_builds_signs_and_sends_signed_settlement_plan() {
+        let settlement_authority = keypair(8);
+        let payer = keypair(9);
+        let base_mint = pubkey(11);
+        let quote_mint = pubkey(12);
+        let market_config = settlement_client::market_config_pda(base_mint, quote_mint).0;
+        let plan = SettlementInstructionPlanner::default().plan_signed_settlement(
+            SignedSettlementRequest {
+                settlement_authority: settlement_authority.pubkey(),
+                base_mint,
+                quote_mint,
+                buyer: pubkey(2),
+                seller: pubkey(3),
+                payer: payer.pubkey(),
+                args: signed_fill_args(market_config),
+            },
+        );
+        let mut submitter = RpcSubmitter::new(
+            FixedBlockhashProvider(Ok(blockhash(1))),
+            InMemoryTransactionSigner::new(vec![settlement_authority, payer]),
+            RecordingTransactionSender::new(Ok(SubmittedTransaction {
+                signature: [44; 64],
+            })),
+        );
+
+        let submitted = submitter.submit(plan).unwrap();
+
+        assert_eq!(submitted.signature, [44; 64]);
+        assert_eq!(submitter.transaction_sender.sent_count, 1);
+    }
+
+    #[test]
+    fn rpc_submitter_builds_signs_and_sends_cancel_plan() {
+        let trader = keypair(2);
+        let base_mint = pubkey(11);
+        let quote_mint = pubkey(12);
+        let market_config = settlement_client::market_config_pda(base_mint, quote_mint).0;
+        let plan = SettlementInstructionPlanner::default().plan_cancel_signed_order(
+            CancelSignedOrderRequest {
+                trader: trader.pubkey(),
+                base_mint,
+                quote_mint,
+                payer: trader.pubkey(),
+                order_hash: [9; 32],
+                order: signed_order(
+                    market_config,
+                    trader.pubkey(),
+                    spot_settlement::SignedOrderSide::Bid,
+                ),
+            },
+        );
+        let mut submitter = RpcSubmitter::new(
+            FixedBlockhashProvider(Ok(blockhash(1))),
+            InMemoryTransactionSigner::new(vec![trader]),
+            RecordingTransactionSender::new(Ok(SubmittedTransaction {
+                signature: [45; 64],
+            })),
+        );
+
+        let submitted = submitter.submit(plan).unwrap();
+
+        assert_eq!(submitted.signature, [45; 64]);
+        assert_eq!(submitter.transaction_sender.sent_count, 1);
+    }
+
+    #[test]
+    fn rpc_submitter_rejects_missing_required_signer() {
+        let settlement_authority = keypair(8);
+        let payer = keypair(9);
+        let base_mint = pubkey(11);
+        let quote_mint = pubkey(12);
+        let market_config = settlement_client::market_config_pda(base_mint, quote_mint).0;
+        let plan = SettlementInstructionPlanner::default().plan_signed_settlement(
+            SignedSettlementRequest {
+                settlement_authority: settlement_authority.pubkey(),
+                base_mint,
+                quote_mint,
+                buyer: pubkey(2),
+                seller: pubkey(3),
+                payer: payer.pubkey(),
+                args: signed_fill_args(market_config),
+            },
+        );
+        let mut submitter = RpcSubmitter::new(
+            FixedBlockhashProvider(Ok(blockhash(1))),
+            InMemoryTransactionSigner::new(vec![settlement_authority]),
+            RecordingTransactionSender::new(Ok(SubmittedTransaction {
+                signature: [46; 64],
+            })),
+        );
+
+        let error = submitter.submit(plan).unwrap_err();
+
+        assert_eq!(
+            error,
+            SubmissionError::MissingRequiredSigner(payer.pubkey())
+        );
+        assert_eq!(submitter.transaction_sender.sent_count, 0);
+    }
+
+    #[test]
+    fn rpc_submitter_propagates_blockhash_failure() {
+        let trader = keypair(2);
+        let base_mint = pubkey(11);
+        let quote_mint = pubkey(12);
+        let market_config = settlement_client::market_config_pda(base_mint, quote_mint).0;
+        let plan = SettlementInstructionPlanner::default().plan_cancel_signed_order(
+            CancelSignedOrderRequest {
+                trader: trader.pubkey(),
+                base_mint,
+                quote_mint,
+                payer: trader.pubkey(),
+                order_hash: [9; 32],
+                order: signed_order(
+                    market_config,
+                    trader.pubkey(),
+                    spot_settlement::SignedOrderSide::Bid,
+                ),
+            },
+        );
+        let mut submitter = RpcSubmitter::new(
+            FixedBlockhashProvider(Err(SubmissionError::BlockhashUnavailable(
+                "rpc unavailable".to_string(),
+            ))),
+            InMemoryTransactionSigner::new(vec![trader]),
+            RecordingTransactionSender::new(Ok(SubmittedTransaction {
+                signature: [47; 64],
+            })),
+        );
+
+        let error = submitter.submit(plan).unwrap_err();
+
+        assert_eq!(
+            error,
+            SubmissionError::BlockhashUnavailable("rpc unavailable".to_string())
+        );
+        assert_eq!(submitter.transaction_sender.sent_count, 0);
+    }
+
+    #[test]
+    fn rpc_submitter_propagates_sender_failure() {
+        let trader = keypair(2);
+        let base_mint = pubkey(11);
+        let quote_mint = pubkey(12);
+        let market_config = settlement_client::market_config_pda(base_mint, quote_mint).0;
+        let plan = SettlementInstructionPlanner::default().plan_cancel_signed_order(
+            CancelSignedOrderRequest {
+                trader: trader.pubkey(),
+                base_mint,
+                quote_mint,
+                payer: trader.pubkey(),
+                order_hash: [9; 32],
+                order: signed_order(
+                    market_config,
+                    trader.pubkey(),
+                    spot_settlement::SignedOrderSide::Bid,
+                ),
+            },
+        );
+        let mut submitter = RpcSubmitter::new(
+            FixedBlockhashProvider(Ok(blockhash(1))),
+            InMemoryTransactionSigner::new(vec![trader]),
+            RecordingTransactionSender::new(Err(SubmissionError::Rejected(
+                "transaction rejected".to_string(),
+            ))),
+        );
+
+        let error = submitter.submit(plan).unwrap_err();
+
+        assert_eq!(
+            error,
+            SubmissionError::Rejected("transaction rejected".to_string())
+        );
+        assert_eq!(submitter.transaction_sender.sent_count, 1);
     }
 }
